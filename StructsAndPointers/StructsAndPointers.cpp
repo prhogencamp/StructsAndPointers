7627bf6d69218572c82bbb75df0f2df1c0946dc7
Patrick <ph68040@email.vccs.edu>
2025-02-24 18:17:41 -0500

    Added ReverseArray solution

--- StructsAndPointers/StructsAndPointers.cpp
+++ StructsAndPointers/StructsAndPointers.cpp
@@ -1,20 +1,54 @@
-// StructsAndPointers.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// ReverseArray.cpp : This program will take an input array, reverse its order, and output the new array.
 //
 
 #include <iostream>
 
+using namespace std;
+
+
+//The following function takes the input integers and size as parameters to reverse their order by use of the temporary variable
+int* reversedArray(int arr[], int size);
+
+//The following function will use the size variable to iterate through the array and print the integers in accordance with the reversedArray function's output
+void print(int const arr[], int size);
+
 int main()
 {
-    std::cout << "Hello World!\n";
+	int size;
+	cout << "How many integers are in your array?";
+	cin >> size;
+
+	int* arr = new int[size];
+
+	cout << "Enter " << size << " integers:" << endl;
+	for (int i = 0; i < size; i++) {
+		cin >> arr[i];
+	}
+
+
+	int* revArr = reversedArray(arr, size);
+	print(revArr, size);
+
+	delete[] arr;
+
+	return 0;
+
+
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+int* reversedArray(int arr[], int size) {
+	for (int i = 0; i < size / 2; i++) {
+		int temporary = arr[i];
+		arr[i] = arr[size - 1 - i];
+		arr[size - 1 - i] = temporary;
+	}
+	return arr;
+}
+void print(int const arr[], int size) {
+	cout << "The reversed array integers are: " << endl;
+	for (int i = 0; i < size; i++) {
+		cout << arr[i] << " ";
+	}
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+	cout << endl;
+}
